76364f14f0cc6922a9ffe9fe566920945fd2908b
Utsav Patel <utsav.kalash131297@gmail.com>
2021-12-07 19:36:39 -0500

    Bug fix.

--- model_architectures.py
+++ model_architectures.py
@@ -58,16 +58,17 @@ def create_model_project1_dense_20x20():
 def create_model_project1_cnn_20x20():
 
     model = models.Sequential()
-    model.add(layers.Conv2D(32, (3, 3), padding='same', activation='relu', input_shape=(3, 20, 20)))
+    model.add(layers.Conv2D(32, (3, 3), padding='same', activation='relu', data_format="channels_first",
+                            input_shape=(3, 20, 20)))
     model.add(layers.MaxPooling2D((2, 2), data_format="channels_first"))
-    model.add(layers.Conv2D(64, (3, 3), padding='same', activation='relu'))
+    model.add(layers.Conv2D(64, (3, 3), padding='same', activation='relu', data_format="channels_first"))
     model.add(layers.MaxPooling2D((2, 2), data_format="channels_first"))
-    model.add(layers.Conv2D(64, (3, 3), padding='same', activation='relu'))
+    model.add(layers.Conv2D(64, (3, 3), padding='same', activation='relu', data_format="channels_first"))
     model.add(layers.Flatten())
     model.add(layers.Dense(128, activation='relu'))
     model.add(layers.Dense(5, activation='softmax'))
 
-    loss_fn = tf.keras.losses.CategoricalCrossentropy(from_logits=True)
+    loss_fn = tf.keras.losses.CategoricalCrossentropy()
     adam = tf.keras.optimizers.Adam(learning_rate=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-07, amsgrad=False,
                                     name='Adam')
 
