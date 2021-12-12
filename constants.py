2033096dc4a1e84f95c83108994ad64087d24097
Utsav Patel <utsav.kalash131297@gmail.com>
2021-12-12 17:15:57 -0500

    Updated constants.py and test file.

--- constants.py
+++ constants.py
@@ -4,20 +4,22 @@ PROJECT_PATH = os.path.dirname(__file__)
 PROJECT_NO = 1
 ARCHITECTURE_TYPE = 'cnn'
 
-NUM_COLS = 10
-NUM_ROWS = 10
+NUM_COLS = 20
+NUM_ROWS = 20
 
 TRAINED_MODEL_NUM_ROWS = 50
 TRAINED_MODEL_NUM_COLS = 50
 INF = 1e9
 
-FILE_PREFIX = "50x50"
+FILE_PREFIX = "20x20_new"
 CHECKPOINT_FILEPATH = os.path.join(PROJECT_PATH, "checkpoints", "project" + str(PROJECT_NO), ARCHITECTURE_TYPE,
                                    FILE_PREFIX, FILE_PREFIX + "-{epoch:04d}.ckpt")
 DATA_PATH = os.path.join(PROJECT_PATH, "data", "project" + str(PROJECT_NO), ARCHITECTURE_TYPE, FILE_PREFIX + ".pkl")
 
-STATE_OF_THE_ART_MODEL_PROJECT1_DENSE_CHECKPOINT_PATH = os.path.join(PROJECT_PATH, "checkpoints", "project1", "dense")
-STATE_OF_THE_ART_MODEL_PROJECT1_CNN_CHECKPOINT_PATH = os.path.join(PROJECT_PATH, "checkpoints", "project1", "cnn")
+STATE_OF_THE_ART_MODEL_PROJECT1_DENSE_CHECKPOINT_PATH = os.path.join(PROJECT_PATH, "checkpoints", "project1", "dense",
+                                                                     FILE_PREFIX)
+STATE_OF_THE_ART_MODEL_PROJECT1_CNN_CHECKPOINT_PATH = os.path.join(PROJECT_PATH, "checkpoints", "project1", "cnn",
+                                                                   FILE_PREFIX)
 
 STARTING_POSITION_OF_AGENT = (0, 0)
 GOAL_POSITION_OF_AGENT = (NUM_ROWS - 1, NUM_COLS - 1)
@@ -29,10 +31,10 @@ UNVISITED_NUMBER = 0
 BLOCKED_NUMBER = -1
 UNBLOCKED_NUMBER = 1
 TARGET_CANNOT_BE_REACHED_NUMBER = 4
-UNBLOCKED_WEIGHT = 10
+UNBLOCKED_WEIGHT = 5
 
-NEIGHBOR_WEIGHT = 25
-CURRENT_CELL_WEIGHT = 300
+NEIGHBOR_WEIGHT = 10
+CURRENT_CELL_WEIGHT = 100
 
 FLAT_FALSE_NEGATIVE_RATE = 0.2
 HILLY_FALSE_NEGATIVE_RATE = 0.5
--- tests/test1.py
+++ tests/test1.py
@@ -9,7 +9,7 @@ from constants import NUM_ROWS, NUM_COLS, STARTING_POSITION_OF_AGENT, GOAL_POSIT
 from src.Maze import Maze
 from helpers.helper import generate_grid_with_probability_p, compute_heuristics, manhattan_distance, repeated_forward, \
     ml_agent_dfs, compute_trajectory_length_from_path, repeated_forward_astar
-from model_architectures import create_model_project1_cnn_20x20, create_model_project1_dense_20x20
+from model_architectures import create_model_project1_cnn_50x50, create_model_project1_dense_10x10
 
 # Just to check how much time the code took
 print('Start running this file at', datetime.now().strftime("%m-%d-%Y %H-%M-%S"))
@@ -21,12 +21,12 @@ num_uniform_samples = 1
 num_times_run_for_each_probability = 1
 
 latest = tf.train.latest_checkpoint(STATE_OF_THE_ART_MODEL_PROJECT1_DENSE_CHECKPOINT_PATH)
-model1 = create_model_project1_dense_20x20()
+model1 = create_model_project1_dense_10x10()
 model1.load_weights(latest)
 
-latest = tf.train.latest_checkpoint(STATE_OF_THE_ART_MODEL_PROJECT1_CNN_CHECKPOINT_PATH)
-model2 = create_model_project1_cnn_20x20()
-model2.load_weights(latest)
+# latest = tf.train.latest_checkpoint(STATE_OF_THE_ART_MODEL_PROJECT1_CNN_CHECKPOINT_PATH)
+# model2 = create_model_project1_cnn_50x50()
+# model2.load_weights(latest)
 
 # List of probability values
 list_of_probability_values = np.linspace(start_value_of_probability, end_value_of_probability, num_uniform_samples)
@@ -42,20 +42,20 @@ def parallel_process_for_each_probability(p):
     # Call to repeated forward A*
     print('Running A*')
     final_paths, is_solvable1 = repeated_forward(mazes[0], full_maze, None, STARTING_POSITION_OF_AGENT,
-                                                GOAL_POSITION_OF_AGENT)[:2]
+                                                 GOAL_POSITION_OF_AGENT)[:2]
     print('Running for dense')
     trajectory_length1, is_solvable2 = ml_agent_dfs(mazes[1], full_maze, STARTING_POSITION_OF_AGENT,
-                                                   GOAL_POSITION_OF_AGENT, project_no=1, architecture_type='dense',
-                                                   model=model1)
-    print('Running for cnn')
-    trajectory_length2, is_solvable3 = ml_agent_dfs(mazes[2], full_maze, STARTING_POSITION_OF_AGENT,
-                                                   GOAL_POSITION_OF_AGENT, project_no=1, architecture_type='cnn',
-                                                   model=model2)
+                                                    GOAL_POSITION_OF_AGENT, project_no=1, architecture_type='dense',
+                                                    model=model1)
+    # print('Running for cnn')
+    # trajectory_length2, is_solvable3 = ml_agent_dfs(mazes[2], full_maze, STARTING_POSITION_OF_AGENT,
+    #                                                 GOAL_POSITION_OF_AGENT, project_no=1, architecture_type='cnn',
+    #                                                 model=model2)
     print('Running for comparison')
-    comparison = repeated_forward_astar(full_maze, STARTING_POSITION_OF_AGENT, GOAL_POSITION_OF_AGENT, model1, model2)
+    comparison = repeated_forward_astar(full_maze, STARTING_POSITION_OF_AGENT, GOAL_POSITION_OF_AGENT, model1, None)
 
-    return [(compute_trajectory_length_from_path(final_paths), is_solvable1, trajectory_length1, is_solvable2,
-             trajectory_length2, is_solvable3), comparison]
+    return [(compute_trajectory_length_from_path(final_paths), is_solvable1, trajectory_length1, is_solvable2), comparison]
+             # trajectory_length2, is_solvable3), comparison]
 
 
 if __name__ == "__main__":
@@ -69,7 +69,7 @@ if __name__ == "__main__":
             print('Probability:', probability_of_having_block, 'Number of run:', ind)
             result = parallel_process_for_each_probability(probability_of_having_block)
             trajectory_lengths.append(result[0])
-            comparisons.append(result[1])
+            # comparisons.append(result[1])
             print(result)
 
     with open(os.path.join(PROJECT_PATH, 'results', 'project1.pkl'), 'wb') as f:
