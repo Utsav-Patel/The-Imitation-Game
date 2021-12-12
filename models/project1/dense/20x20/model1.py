642ae6551e91c8e8fffcdf6b008645688430e89d
Utsav Patel <utsav.kalash131297@gmail.com>
2021-12-12 18:49:21 -0500

    Changed model file for 20x20 and 50x50.

--- models/project1/dense/20x20/model1.py
+++ models/project1/dense/20x20/model1.py
@@ -4,46 +4,53 @@ from sklearn.model_selection import train_test_split
 import tensorflow as tf
 from tensorflow.keras.utils import to_categorical
 
-from constants import NUM_ROWS, NUM_COLS, CHECKPOINT_FILEPATH, DATA_PATH, X, Y, NEIGHBOR_WEIGHT, CURRENT_CELL_WEIGHT
+from constants import NUM_ROWS, NUM_COLS, CHECKPOINT_FILEPATH, DATA_PATH, X, Y, NEIGHBOR_WEIGHT, CURRENT_CELL_WEIGHT,\
+    VALIDATION_TEST_PATH
 from model_architectures import create_model_project1_dense_20x20
 from helpers.helper import check
 
-open_file = open(DATA_PATH, "rb")
-loaded_list = pickle.load(open_file)
-open_file.close()
 
-print("Successfully loaded data from pickle file")
+def prepare_dataset(path):
 
-input_list = list()
-output_list = list()
-current_position_list = list()
+    print('Loading data from', path)
+    open_file = open(path, "rb")
+    loaded_list = pickle.load(open_file)
+    open_file.close()
 
-for dct in loaded_list:
-    input_list.append(dct['input'])
-    output_list.append(dct['output'])
-    current_position_list.append(dct['current_pos'])
+    print("Successfully loaded data from pickle file")
 
-for ind in range(len(input_list)):
-    input_list[ind][current_position_list[ind][0]][current_position_list[ind][1]] = CURRENT_CELL_WEIGHT
-    for ind2 in range(len(X)):
-        neighbor = (current_position_list[ind][0] + X[ind2], current_position_list[ind][1] + Y[ind2])
-        if check(neighbor, NUM_ROWS, NUM_COLS):
-            input_list[ind][neighbor[0]][neighbor[1]] *= NEIGHBOR_WEIGHT
+    input_list = list()
+    output_list = list()
+    current_position_list = list()
 
-input_numpy = np.array(input_list)
-input_numpy = input_numpy.reshape(input_numpy.shape[0], -1)
-# input_numpy = np.hstack((np.zeros(input_numpy.shape[0]).reshape(-1, 1), input_numpy))
+    for dct in loaded_list:
+        input_list.append(dct['input'])
+        output_list.append(dct['output'])
+        current_position_list.append(dct['current_pos'])
 
-output_numpy = np.array(output_list)
-output_numpy = output_numpy.reshape(output_numpy.shape[0])
-output_numpy = to_categorical(output_numpy)
+    for ind in range(len(input_list)):
+        input_list[ind][current_position_list[ind][0]][current_position_list[ind][1]] = CURRENT_CELL_WEIGHT
+        for ind2 in range(len(X)):
+            neighbor = (current_position_list[ind][0] + X[ind2], current_position_list[ind][1] + Y[ind2])
+            if check(neighbor, NUM_ROWS, NUM_COLS):
+                input_list[ind][neighbor[0]][neighbor[1]] *= NEIGHBOR_WEIGHT
 
-print("Input shape", input_numpy.shape)
-print("Output shape", output_numpy.shape)
-print('Starting training')
+    input_numpy = np.array(input_list)
+    input_numpy = input_numpy.reshape(input_numpy.shape[0], -1)
+    # input_numpy = np.hstack((np.zeros(input_numpy.shape[0]).reshape(-1, 1), input_numpy))
 
-X_train, X_test, y_train, y_test = train_test_split(input_numpy, output_numpy, test_size=0.05, random_state=81)
-X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.05, random_state=81)
+    output_numpy = np.array(output_list)
+    output_numpy = output_numpy.reshape(output_numpy.shape[0])
+    output_numpy = to_categorical(output_numpy)
+
+    return input_numpy, output_numpy
+
+
+X_train, y_train = prepare_dataset(DATA_PATH)
+X_val, y_val = prepare_dataset(VALIDATION_TEST_PATH)
+
+# X_train, X_test, y_train, y_test = train_test_split(input_numpy, output_numpy, test_size=0.05, random_state=81)
+X_test, X_val, y_test, y_val = train_test_split(X_val, y_val, test_size=0.50, random_state=81)
 
 print("X train shape", X_train.shape)
 print("y train shape", y_train.shape)
@@ -64,7 +71,7 @@ model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
     save_freq='epoch'
 )
 
-history = model.fit(X_train, y_train, epochs=20, batch_size=128, validation_data=(X_val, y_val),
+history = model.fit(X_train, y_train, epochs=30, batch_size=512, validation_data=(X_val, y_val),
                     callbacks=[model_checkpoint_callback])
 
 print(history.history)
