e9f7c3f514c13c1d3b4759bb51ef857736a83f98
Utsav Patel <utsav.kalash131297@gmail.com>
2021-12-13 14:24:59 -0500

    Updated model file to train project1 CNN architecture.

--- models/project1/cnn/model1.py
+++ models/project1/cnn/model1.py
@@ -4,37 +4,44 @@ from sklearn.model_selection import train_test_split
 import tensorflow as tf
 from tensorflow.keras.utils import to_categorical
 
-from constants import CHECKPOINT_FILEPATH, DATA_PATH
-from model_architectures import create_model_project1_cnn_50x50
+from constants import CHECKPOINT_FILEPATH, DATA_PATH, VALIDATION_TEST_PATH
+from model_architectures import create_model_project1_cnn_20x20
 from DataGenerator import DataGenerator
 
-open_file = open(DATA_PATH, "rb")
-loaded_list = pickle.load(open_file)
-open_file.close()
 
-print("Successfully loaded data from pickle file")
+def prepare_dataset(path):
+    open_file = open(path, "rb")
+    loaded_list = pickle.load(open_file)
+    open_file.close()
 
-input_list = list()
-output_list = list()
+    print("Successfully loaded data from pickle file", path)
 
-for dct in loaded_list:
-    input_list.append({'input': dct['input'], 'current_pos': dct['current_pos']})
-    output_list.append(dct['output'])
+    input_list = list()
+    output_list = list()
 
-# input_numpy = np.array(input_list)
-# print(input_numpy.shape)
-# # input_numpy = input_numpy.reshape(input_numpy.shape[0], -1)
+    for dct in loaded_list:
+        input_list.append({'input': dct['input'], 'current_pos': dct['current_pos']})
+        output_list.append(dct['output'])
 
-output_numpy = np.array(output_list)
-output_numpy = output_numpy.reshape(output_numpy.shape[0])
-output_numpy = to_categorical(output_numpy)
+    # input_numpy = np.array(input_list)
+    # print(input_numpy.shape)
+    # # input_numpy = input_numpy.reshape(input_numpy.shape[0], -1)
+
+    output_numpy = np.array(output_list)
+    output_numpy = output_numpy.reshape(output_numpy.shape[0])
+    output_numpy = to_categorical(output_numpy)
+
+    return input_list, output_numpy
+
 
 # print("Input shape", input_numpy.shape)
 # print("Output shape", output_numpy.shape)
 # print('Starting training')
 
-X_train, X_test, y_train, y_test = train_test_split(input_list, output_numpy, test_size=0.05, random_state=81)
-X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.05, random_state=81)
+X_train, y_train = prepare_dataset(DATA_PATH)
+X_val, y_val = prepare_dataset(VALIDATION_TEST_PATH)
+
+X_val, X_test, y_val, y_test = train_test_split(X_val, y_val, test_size=0.50, random_state=81)
 
 # print("X train shape", X_train.shape)
 # print("y train shape", y_train.shape)
@@ -47,7 +54,7 @@ training_generator = DataGenerator(X_train, y_train)
 validation_generator = DataGenerator(X_val, y_val)
 testing_generator = DataGenerator(X_test, y_test)
 
-model = create_model_project1_cnn_50x50()
+model = create_model_project1_cnn_20x20()
 model.summary()
 
 model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
@@ -64,4 +71,3 @@ history = model.fit(training_generator, epochs=30, validation_data=validation_ge
 
 print(history.history)
 model.evaluate(testing_generator, verbose=2)
-
